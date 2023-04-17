92400ae7507342e25e4af40b55b9c276b2bce8b9
Cyryl Gotkowicz <cyrylgotkowicz@gmail.com>
2023-04-17 15:08:36 +0100

    dairy_entry reading_chunk tests

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -1,35 +1,39 @@
 class DiaryEntry
-    def initialize(title, contents) # Initialize method, called when a new DiaryEntry object is created
-      @title = title                # Save the title as an instance variable (object's memory)
-      @contents = contents          # Save the contents as an instance variable (object's memory)
-    end
-  
-    def title
-      @title                        # Returns the title as a string
-    end
-  
-    def contents
-      @contents                     # Returns the contents as a string
-    end
-  
-    def count_words
-      @contents.split.count         # Splits the contents into words and counts them, then returns the count as an integer
-    end
-  
-    def reading_time(wpm) # wpm is an integer representing the number of words the user can read per minute
-      fail "word per minute must be above 0." unless wpm.positive?
-      (count_words / wpm.to_f).ceil # Calculate the reading time in minutes and round it up, then return the result as an integer
-    end
-  
-    def reading_chunk(wpm, minutes) # wpm is an integer, minutes is an integer
-      words_to_read = wpm * minutes # Calculate how many words the user can read in the given minutes
-      words = @contents.split       # Split the contents into words
-  
-      # Get a chunk of words the user can read in the given time and join them into a string
-      chunk = words.shift(words_to_read).join(' ')
-      @contents = words.join(' ')    # Update the contents to exclude the chunk that was read
-  
-      chunk                          # Return the chunk as a string
-    end
+  def initialize(title, contents) # Initialize method, called when a new DiaryEntry object is created
+    @title = title                # Save the title as an instance variable (object's memory)
+    @contents = contents
+    @current_position = 0          # Save the contents as an instance variable (object's memory)
   end
-  
+
+  def title
+    @title                        # Returns the title as a string
+  end
+
+  def contents
+    @contents                     # Returns the contents as a string
+  end
+
+  def count_words
+    @contents.split.size         # Splits the contents into words and counts them, then returns the count as an integer
+  end
+
+  def reading_time(wpm) # wpm is an integer representing the number of words the user can read per minute
+    fail "word per minute must be above 0." unless wpm.positive?
+    (count_words / wpm.to_f).ceil # Calculate the reading time in minutes and round it up, then return the result as an integer
+  end
+
+  def reading_chunk(wpm, minutes) # wpm is an integer, minutes is an integer
+    words_to_read = wpm * minutes # Calculate how many words the user can read in the given minutes
+    words = @contents.split       # Split the contents into words
+
+    # Get a chunk of words the user can read in the given time and join them into a string
+    start_position = @current_position    # Update the contents to exclude the chunk that was read
+    end_position = start_position + words_to_read - 1
+    end_position = [end_position, words.size - 1].min
+
+    @current_position = end_position + 1
+    @current_position = 0 if @current_position >= words.size
+    words[start_position..end_position].join(" ")
+    # Return the chunk as a string
+  end
+end
--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -1,48 +1,73 @@
 #file: diary_entry_spec.rb
-require 'diary_entry'
+require "diary_entry"
 
 RSpec.describe DiaryEntry do
-    it 'Returns the title as a string' do
-        diary_entry = DiaryEntry.new("my_title", "my_contents")
-        expect(diary_entry.title).to eq("my_title")
-        expect(diary_entry.contents).to eq("my_contents")
+  it "Returns the title as a string" do
+    diary_entry = DiaryEntry.new("my_title", "my_contents")
+    expect(diary_entry.title).to eq("my_title")
+    expect(diary_entry.contents).to eq("my_contents")
+  end
+
+  describe "#count_words" do
+    context "Returns the contents as a string" do
+      it "return words count" do
+        diary_entry = DiaryEntry.new("my_title", "one")
+        expect(diary_entry.count_words).to eq(1)
+      end
+
+      it "return 0" do
+        diary_entry = DiaryEntry.new("my_title", "")
+        expect(diary_entry.count_words).to eq(0)
+      end
+    end
+  end
+
+  describe "#reading_time" do
+    context "given a wpm of sensible numbers(200)" do
+      it "calculate how long it takes to read by round it up" do
+        diary_entry = DiaryEntry.new("my_title", "one " * 550)
+        expect(diary_entry.reading_time(200)).to eq(3)
+      end
     end
 
-    describe "#count_words" do
-        context "Returns the contents as a string" do
-            it 'return words count' do
-                diary_entry = DiaryEntry.new("my_title", "one")
-                expect(diary_entry.count_words).to eq(1)
-            end
+    context "given a wpm of 0" do
+      it "fails" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        expect { diary_entry.reading_time(0) }.to raise_error "word per minute must be above 0."
+      end
+    end
+  end
 
-            it 'return 0' do
-                diary_entry = DiaryEntry.new("my_title", "")
-                expect(diary_entry.count_words).to eq(0)
-            end
-        end
+  describe "#reading_chunk" do
+    context "when the content are readable within the time" do
+      it "Return the chunk as a string" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        expect(diary_entry.reading_chunk(200, 1)).to eq("one two three")
+      end
     end
 
-    describe "#reading_time" do
-        context "given a wpm of sensible numbers(200)" do
-            it 'calculate how long it takes to read by round it up' do
-                diary_entry = DiaryEntry.new("my_title", "one " * 550)
-                expect(diary_entry.reading_time(200)).to eq(3)
-            end
-        end  
-
-        context "given a wpm of 0" do
-            it 'fails' do
-                diary_entry = DiaryEntry.new("my_title", "one two three")
-                expect { diary_entry.reading_time(0)}.to raise_error "word per minute must be above 0."
-            end
-        end
-
-    describe "#reading_chunk" do
-        context "when the content are readable within the time" do
-            it 'Return the chunk as a string' do
-                diary_entry = DiaryEntry.new("my_title", "one two three")
-                expect(diary_entry.reading_chunk(200, 1)).to eq("one two three")
-            end    
-        end
+    context "when the contents are not readable within the time" do
+      it "return a readable chunk" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        expect(diary_entry.reading_chunk(2, 1)).to eq("one two")
+      end
     end
+
+    context "when reading_chunk is called a second time" do
+      it "return second chunk" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        expect(diary_entry.reading_chunk(2, 1)).to eq("three")
+      end
+    end
+
+    context "when all chunks are called, restart from beginning" do
+      it "return first chunk" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        diary_entry.reading_chunk(2, 1)
+        expect(diary_entry.reading_chunk(2, 1)).to eq "one two"
+      end
+    end
+  end
 end
